b2a6a60a7dd4b0cd473e027f9c5846db563e499b
rodelrod <rodelrod@hotmail.com>
2018-05-07 01:48:21 +0200

    Mark spacemacs light and dark themes as safe

--- init.el
+++ init.el
@@ -384,6 +384,10 @@ you should place your code here."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" default)))
+ '(evil-want-Y-yank-to-eol nil)
  '(org-agenda-custom-commands
    (quote
     (("n" "Agenda, NEXT, and other TODOs"
